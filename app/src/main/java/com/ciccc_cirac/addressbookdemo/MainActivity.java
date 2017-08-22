020215bed1c4601350ef9763152ea75d10d1efd9
Mitali Pattani <sonarmitali19@gmail.com>
2017-08-22 12:19:35 -0700

    Added the Addedot Fragment

--- app/src/main/java/com/ciccc_cirac/addressbookdemo/MainActivity.java
+++ app/src/main/java/com/ciccc_cirac/addressbookdemo/MainActivity.java
@@ -1,25 +1,35 @@
 package com.ciccc_cirac.addressbookdemo;
 
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
+        displayAddEditFragment(R.id.fragmentContainer ,null );
     }
-
+    // display fragment for adding a new or editing an existing contact
+    // ViewID is layout id & it should be the container
+    // contacturi is the path for contentProvider
+    public void displayAddEditFragment(int ViewId ,  Uri contacturi)
+    {
+        AddEditFragment addEditFragment =
+                new AddEditFragment();
+        // Create the fragment using FragmentTransaction
+        FragmentTransaction fragmentTransaction =
+                getSupportFragmentManager()
+                .beginTransaction();
+        fragmentTransaction.replace(ViewId, addEditFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
 
 }
